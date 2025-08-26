411ebbd98d5eaacae0cae80767a7d2f519f22f75
Josh King <162310653+jjjmdev@users.noreply.github.com>
2025-08-26 16:49:19 +0800

    Add UI handling for Item Modal

--- src/components/ItemModal.jsx
+++ src/components/ItemModal.jsx
@@ -1,4 +1,82 @@
+import { useState } from 'react'
+import { getPersons } from '../data.js'
+
 export default function ItemModal() {
+	const persons = getPersons()
+
+	const [payerCount, setPayerCount] = useState(persons.length && 1)
+	const [payerArray, setPayerArray] = useState(
+		[...Array(persons.length)].map((_, key) => {
+			return <PayerDefaultElement id={key} key={key} />
+		})
+	)
+	const limitAdd = payerCount === persons.length
+
+	const handleAddClick = (e) => {
+		e.preventDefault()
+
+		setPayerCount(() => payerCount + 1)
+	}
+
+	function PayerDefaultElement({ id }) {
+		return (
+			<div className='flex gap-2 ' style={{ width: '100%' }}>
+				<fieldset className='fieldset flex-1'>
+					<legend className='fieldset-legend flex justify-between w-full'>
+						<span>Sino nagbayad?</span>
+						{id > 0 && (
+							<button
+								className='btn btn-soft btn-xs btn-warning'
+								onClick={() => handleDeleteClick(id)}
+							>
+								<svg
+									xmlns='http://www.w3.org/2000/svg'
+									width='16'
+									height='16'
+									fill='currentColor'
+									class='bi bi-trash'
+									viewBox='0 0 16 16'
+								>
+									<path d='M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z' />
+									<path d='M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z' />
+								</svg>
+							</button>
+						)}
+					</legend>
+					<select className='select'>
+						<option disabled={true}>Pumili</option>
+						{persons.map((person) => {
+							return <option>{person.name}</option>
+						})}
+					</select>
+				</fieldset>
+
+				<fieldset className='fieldset flex-1 self-end'>
+					<legend className='fieldset-legend'>Magkano?</legend>
+					<label className='input'>
+						<span className='label'>₱</span>
+						<input type='text' placeholder='49' />
+					</label>
+				</fieldset>
+			</div>
+		)
+	}
+
+	const renderPayerArray = () => {
+		console.log(payerArray, payerCount, limitAdd)
+		return payerArray.slice(0, payerCount)
+	}
+
+	const handleDeleteClick = (index) => {
+		const newArray = [
+			...payerArray.slice(0, index),
+			...payerArray.slice(index + 1),
+			payerArray[index],
+		]
+		setPayerArray(newArray)
+		setPayerCount((payerCount) => payerCount - 1)
+	}
+
 	return (
 		<>
 			<button
@@ -8,7 +86,7 @@ export default function ItemModal() {
 				+ Item
 			</button>
 			<dialog id='add_item' className='modal'>
-				<div className='modal-box'>
+				<div className='modal-box m-2'>
 					{/* <h3 className='font-bold text-lg'>Add Item</h3> */}
 					<div className='flex gap-2 ' style={{ width: '100%' }}>
 						<fieldset className='fieldset flex-1'>
@@ -25,32 +103,23 @@ export default function ItemModal() {
 						</fieldset>
 					</div>
 
-					<div className='flex gap-2 ' style={{ width: '100%' }}>
-						<fieldset className='fieldset flex-1'>
-							<legend className='fieldset-legend'>Sino nagbayad?</legend>
-							<select defaultValue='Pick a browser' className='select'>
-								<option disabled={true}>Pumili</option>
-								<option>Joshua</option>
-								<option>Pen</option>
-							</select>
-						</fieldset>
-
-						<fieldset className='fieldset flex-1'>
-							<legend className='fieldset-legend'>Magkano?</legend>
-							<label className='input'>
-								<span className='label'>₱</span>
-								<input type='text' placeholder='49' />
-							</label>
-						</fieldset>
-					</div>
+					{renderPayerArray()}
 
 					<div className='modal-action'>
 						<form method='dialog' className='w-full'>
-							<div className='flex justify-between mt-3'>
-								<button className='btn btn-xs self-start'>
+							<div
+								className={
+									'flex mt-3 ' + (limitAdd ? 'justify-end' : 'justify-between')
+								}
+							>
+								<button
+									className='btn btn-xs self-start'
+									onClick={handleAddClick}
+									style={{ display: limitAdd ? 'none' : 'block' }}
+								>
 									+ may iba pang nagbayad
 								</button>
-								<div>
+								<div className='justify-self-end self-end'>
 									<button className='btn btn-soft btn-sm btn-error mr-2'>
 										Cancel
 									</button>
